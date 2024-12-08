6ff7ca251dd6e3ccb6f31dcb710e2181f3e116a8
Imran Hossain <ihossain@vantage.com>
2024-12-08 18:43:11 +0600

    Send message after enter button hit.

--- frontend/src/pages/Auth/Chat/Chat.jsx
+++ frontend/src/pages/Auth/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import useFetchData from '@/hooks/useFetchData';
 import Loader from '@/components/Include/Loader';
 import UserList from '@/components/UserList';
@@ -13,7 +13,7 @@ import socket from "@/helpers/socket";
 
 const ChatList = ({ chats, onSelectChat }) => {
     const { auth } = useAuth();
-    const [showModal,setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     return (
         <div className="w-1/4 bg-gray-800 text-gray-100 h-full border-r border-gray-700">
@@ -22,18 +22,18 @@ const ChatList = ({ chats, onSelectChat }) => {
                     {auth?.user?.name}
                 </div>
                 <div className="flex flex-1 justify-end w-full">
-                    <button 
-                        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2" 
-                        onClick={()=>setShowModal(true)}
+                    <button
+                        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2"
+                        onClick={() => setShowModal(true)}
                     >
                         Create Room
                     </button>
                 </div>
             </div>
-             {showModal && 
+            {showModal &&
                 <Modal>
-                    <UserList onClose={()=>setShowModal(false)}/>
-                </Modal> 
+                    <UserList onClose={() => setShowModal(false)} />
+                </Modal>
             }
             <ul>
                 {chats.map((chat) => (
@@ -41,7 +41,7 @@ const ChatList = ({ chats, onSelectChat }) => {
                         ch._id != auth.user._id &&
                         <li
                             key={ch._id}
-                            onClick={() => onSelectChat({...ch,roomId:chat._id})}
+                            onClick={() => onSelectChat({ ...ch, roomId: chat._id })}
                             className="flex items-center p-4 cursor-pointer hover:bg-gray-700 transition"
                         >
                             <div className="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center text-white text-lg font-bold">
@@ -59,7 +59,7 @@ const ChatList = ({ chats, onSelectChat }) => {
     );
 };
 
-const FileView = ({name}) => {
+const FileView = ({ name }) => {
     const fileLink = `${import.meta.env.VITE_BASE_URL}/chat/file-download/${name}`
     return (
         <div className="flex items-start bg-gray-50 dark:bg-gray-600 rounded-xl p-2 mt-2">
@@ -69,8 +69,8 @@ const FileView = ({name}) => {
                 </span>
             </div>
             <div className="inline-flex self-center items-center">
-            <a
-                href={fileLink}
+                <a
+                    href={fileLink}
                     className="inline-flex self-center items-center p-2 text-sm font-medium text-center text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-600"
                     type="button"
                 >
@@ -93,7 +93,6 @@ const FileView = ({name}) => {
 const ChatBubble = ({ message, name, time, sentByMe, file }) => {
 
     const cls = sentByMe ? 'right-chat' : 'left-chat'
-    console.log('time',time)
 
     return (
         <div className={cls}>
@@ -120,61 +119,59 @@ const ChatBubble = ({ message, name, time, sentByMe, file }) => {
 }
 
 
-const Attachment = ({remove,name}) => (
+const Attachment = ({ remove, name }) => (
     <div className="flex bg-white absolute bottom-[90px] m-1 rounded-md">
-    <div className="grid gap-1 p-3 rounded-md shadow-2xl bg-gray-400">
-      <div className="flex items-center justify-between gap-2">
-        <div className="flex items-center gap-2">
-          <div className="grid gap-1">
-            <h4 className="text-gray-900 text-sm font-normal leading-snug">
-              {name}
-            </h4>
-          </div>
+        <div className="grid gap-1 p-3 rounded-md shadow-2xl bg-gray-400">
+            <div className="flex items-center justify-between gap-2">
+                <div className="flex items-center gap-2">
+                    <div className="grid gap-1">
+                        <h4 className="text-gray-900 text-sm font-normal leading-snug">
+                            {name}
+                        </h4>
+                    </div>
+                </div>
+                <div onClick={remove} className="cursor-pointer">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                    >
+                        <g id="Upload 3">
+                            <path
+                                id="icon"
+                                d="M15 9L12 12M12 12L9 15M12 12L9 9M12 12L15 15M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                                stroke="#D1D5DB"
+                                strokeWidth="1.6"
+                                strokeLinecap="round"
+                            />
+                        </g>
+                    </svg>
+                </div>
+            </div>
         </div>
-       <div onClick={remove} className="cursor-pointer">
-       <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={24}
-          height={24}
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <g id="Upload 3">
-            <path
-              id="icon"
-              d="M15 9L12 12M12 12L9 15M12 12L9 9M12 12L15 15M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-              stroke="#D1D5DB"
-              strokeWidth="1.6"
-              strokeLinecap="round"
-            />
-          </g>
-        </svg>
-       </div>
-      </div>
-    </div>
     </div>
 )
 
 const AttachmentIcon = ({ size = 24, color = 'black' }) => (
     <svg
-  width={size}
-  height={size}
-  viewBox="0 0 24 24"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path
-    fillRule="evenodd"
-    clipRule="evenodd"
-    d="M9 7C9 4.23858 11.2386 2 14 2C16.7614 2 19 4.23858 19 7V15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9C5 8.44772 5.44772 8 6 8C6.55228 8 7 8.44772 7 9V15C7 17.7614 9.23858 20 12 20C14.7614 20 17 17.7614 17 15V7C17 5.34315 15.6569 4 14 4C12.3431 4 11 5.34315 11 7V15C11 15.5523 11.4477 16 12 16C12.5523 16 13 15.5523 13 15V9C13 8.44772 13.4477 8 14 8C14.5523 8 15 8.44772 15 9V15C15 16.6569 13.6569 18 12 18C10.3431 18 9 16.6569 9 15V7Z"
-    fill="black"
-  />
-</svg>
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9 7C9 4.23858 11.2386 2 14 2C16.7614 2 19 4.23858 19 7V15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9C5 8.44772 5.44772 8 6 8C6.55228 8 7 8.44772 7 9V15C7 17.7614 9.23858 20 12 20C14.7614 20 17 17.7614 17 15V7C17 5.34315 15.6569 4 14 4C12.3431 4 11 5.34315 11 7V15C11 15.5523 11.4477 16 12 16C12.5523 16 13 15.5523 13 15V9C13 8.44772 13.4477 8 14 8C14.5523 8 15 8.44772 15 9V15C15 16.6569 13.6569 18 12 18C10.3431 18 9 16.6569 9 15V7Z"
+            fill="black"
+        />
+    </svg>
+);
 
-
-  );
-  
-const ChatDetails = ({ roomId, messages, onSendMessage, onUploadAttachment,children }) => {
+const ChatDetails = ({ roomId, messages, onSendMessage, onUploadAttachment, children }) => {
 
     const [message, setMessage] = useState("");
 
@@ -191,79 +188,92 @@ const ChatDetails = ({ roomId, messages, onSendMessage, onUploadAttachment,child
 
             if (userObj?.name) {
                 name = userObj.name;
+                return;
             }
         })
 
         return name;
     }
 
-    const handleSend = () => {
+    const handleSend = (e) => {
+        e.preventDefault();
+
         if (message.trim()) {
             onSendMessage({ message, roomId, sender_id: auth.user._id });
             setMessage("");
-        }else{
+        } else {
             alert('Please input text')
         }
     };
 
-    useEffect(()=>{
+    // const handleUpload = (e) => {
+    //     const file = e.target.files[0];
+    //     setFile(file)
+    //     onUploadAttachment(e)
+    // }
+
+    useEffect(() => {
         if (messagesEndRef.current) {
             messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
         }
-    },[messages])
+    }, [messages])
 
     return (
         <>
-            {/* Chat Messages */}
             <div className="flex-grow p-4 bg-white overflow-y-auto" ref={messagesEndRef}>
-                {messages.map((msg, idx) => (
-                    <ChatBubble
-                        key={idx}
-                        file={msg.file}
-                        time={msg.timestamp}
-                        message={msg.text}
-                        name={msg.sender_id == auth.user._id ? auth.user.name : findNameBySenderId(msg.sender_id)}
-                        sentByMe={msg.sender_id == auth.user._id}
-                    />
-                ))}
-                {/* <div ref={messagesEndRef}></div> */}
-                
+                {
+                    messages.map((msg, idx) => (
+                        <ChatBubble
+                            key={idx}
+                            file={msg.file}
+                            time={msg.timestamp}
+                            message={msg.text}
+                            name={msg.sender_id == auth.user._id ? auth.user.name : findNameBySenderId(msg.sender_id)}
+                            sentByMe={msg.sender_id == auth.user._id}
+                        />
+                    ))
+                }
             </div>
 
             {/* Input Box */}
-            
+
             <div className="relative">
-            {children}
-                
-            <div className="p-4 bg-gray-300 shadow-md flex items-center border-t border-gray-700 gap-3">
-                {/* Attachment Upload */}
-                <label
-                    htmlFor="attachment"
-                    className="cursor-pointer text-gray-400 hover:text-gray-100 transition"
-                >
-                   <AttachmentIcon/>
-                    <input
-                        type="file"
-                        id="attachment"
-                        onChange={onUploadAttachment}
-                        className="hidden"
-                    />
-                </label>
-                <input
-                    type="text"
-                    placeholder="Type a message..."
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    className="flex-grow px-4 py-4 rounded-lg text-black bg-gray-200 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-               
-                <button
-                    onClick={handleSend}
-                    className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition shadow-md"
-                >
-                    Send
-                </button>
-            </div>
+                {children}
+
+                <div className="p-4 bg-gray-300 shadow-md flex items-center border-t border-gray-700 gap-3">
+                    {/* Attachment Upload */}
+                    <label
+                        htmlFor="attachment"
+                        className="cursor-pointer text-gray-400 hover:text-gray-100 transition"
+                    >
+                        <AttachmentIcon />
+                        <input
+                            id="attachment"
+                            type="file"
+                            onChange={onUploadAttachment}
+                            className="hidden"
+                        />
+                    </label>
+
+                    <form className="w-full flex" onSubmit={handleSend}>
+                        <input
+                            type="text"
+                            required
+                            placeholder="Type a message..."
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            className="mr-5 flex-grow px-4 py-4 rounded-lg text-black bg-gray-200 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+
+                        <button
+                            type="submit"
+                            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition shadow-md"
+                        >
+                            Send
+                        </button>
+                    </form>
+                </div>
+
             </div>
         </>
     );
@@ -285,15 +295,15 @@ export default function Chat() {
     const [messages, setMessages] = useState([]);
 
 
-    const handleChatSelect = async ({...data}) => {
-        
-        const {roomId,name} = data;
+    const handleChatSelect = async ({ ...data }) => {
+
+        const { roomId, name } = data;
 
         try {
             setLoading(true);
             // socket.emit('joinRoom', { roomId });
             const response = await api.get(`/chat/${roomId}`);
-            setSelectedChat({roomId,name});
+            setSelectedChat({ roomId, name });
             setMessages(response?.data?.data?.messages || []);
 
         } catch (err) {
@@ -305,9 +315,9 @@ export default function Chat() {
     };
 
     useEffect(() => {
-       
+
         socket.on("message", (data) => {
-            if(data.roomId == selectedChat?.roomId){
+            if (data.roomId == selectedChat?.roomId) {
                 setMessages((prev) => [...prev, data]);
             }
         });
@@ -316,19 +326,19 @@ export default function Chat() {
             socket.off("message");
         };
 
-    }, [selectedChat,messages])
+    }, [selectedChat, messages])
 
     const handleSendMessage = (messageObj) => {
         const currentTime = Date.now();
         const finalObj = {
-            roomId:selectedChat.roomId,
+            roomId: selectedChat.roomId,
             text: messageObj.message,
             // file: messageObj?.file || "",
             timestamp: currentTime,
             sender_id: messageObj.sender_id
         }
 
-        if(attachment){
+        if (attachment) {
             finalObj.fileName = attachment.name
             finalObj.fileData = attachment
         }
@@ -341,9 +351,9 @@ export default function Chat() {
     const handleUploadAttachment = (event) => {
         const file = event.target.files[0];
         const maxSizeInBytes = 1 * 1024 * 1024; // 1MB
-
+        console.log(event)
         if (file) {
-            
+
             if (file.size > maxSizeInBytes) {
                 alert('File size exceeds 1MB. Please select a smaller file.')
                 return;
@@ -379,7 +389,7 @@ export default function Chat() {
             {
                 loading && <Loader />
             }
-           
+
             {/* Chat List Sidebar */}
             <ChatList chats={chats} onSelectChat={handleChatSelect} />
 
@@ -401,7 +411,7 @@ export default function Chat() {
                             onSendMessage={handleSendMessage}
                             onUploadAttachment={handleUploadAttachment}
                         >
-                           {attachment && <Attachment name={attachment.name} remove={handleAttachmentRemove} />} 
+                            {attachment && <Attachment name={attachment.name} remove={handleAttachmentRemove} />}
                         </ChatDetails>
                     ) : (
                         <div className="h-full flex items-center justify-center text-gray-500">
