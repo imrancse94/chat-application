6966703b4d8883499225a79638238da61e37e08a
Imran Hossain <ihossain@vantage.com>
2025-09-27 16:22:56 +0600

    test comment added

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: 'Something went wrong', error: err.message });
 });
 
-// Routes
+// Routes // test
 app.use('/auth', require('./routes/auth'));
 app.use('/chat', require('./routes/chat'));
 
